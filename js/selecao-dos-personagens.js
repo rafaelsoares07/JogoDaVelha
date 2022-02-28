31c23b04ccb343449888e218e6ac4a26dee4b9f5
Rafael Soares <rafaelsoares1936@gmail.com>
2022-02-28 19:14:45 -0300

    segundo commit

---
+++ js/selecao-dos-personagens.js
@@ -0,0 +1,14 @@
+
+
+let play1 = document.querySelectorAll('p1');
+
+console.log(play1);
+
+play1.forEach((jogador)=>{
+	jogador.addEventListener("click", function(){
+
+		console.log("olaaa")
+
+	})
+
+})
